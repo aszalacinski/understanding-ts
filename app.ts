06eaa73a11b14e510ba185287c8f9b908ab06b86
Aarron Szalacinski <ASzalacinski@corporatespending.com>
2020-01-07 14:03:16 -0500

    lesson 33

--- app.ts
+++ app.ts
@@ -1,16 +1,3 @@
-let userInput: unknown;
-let userName: string;
+const userName = 'Aarron';
 
-userInput = 5;
-userInput = "Aarron";
-
-if (typeof userInput === "string") {
-  userName = userInput;
-}
-
-function generateError(message: string, code: number): never {
-  throw { message: message, errorCode: code };
-  // while (true) {} -- infinite loop
-}
-
-generateError('An error occurred', 500);
+console.log(userName);
