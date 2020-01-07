62baa4c0f7ef5695d9e07bc566a3a6839c1cb176
Aarron Szalacinski <ASzalacinski@corporatespending.com>
2020-01-07 09:34:40 -0500

    lesson 17

--- app.ts
+++ app.ts
@@ -4,7 +4,15 @@
 // } = {
 const person = {
     name: 'Aarron',
-    age: 44
-}
+    age: 44,
+    hobbies: ['Sports', 'Cooking']
+};
+
+let favoriteActivities: string[];
+favoriteActivities = ['Sports'];
 
 console.log(person.name);
+
+for(const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+}
