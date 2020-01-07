37fa499007fdd8a7420a5d7d78c737a5126740c1
Aarron Szalacinski <ASzalacinski@corporatespending.com>
2020-01-07 13:57:22 -0500

    lesson 29

--- app.ts
+++ app.ts
@@ -2,8 +2,15 @@ let userInput: unknown;
 let userName: string;
 
 userInput = 5;
-userInput = 'Aarron';
+userInput = "Aarron";
 
-if(typeof userInput === 'string') {
-    userName = userInput;
+if (typeof userInput === "string") {
+  userName = userInput;
 }
+
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
+  // while (true) {} -- infinite loop
+}
+
+generateError('An error occurred', 500);
