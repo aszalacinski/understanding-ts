bc64cc06854a863f2eeb6615e8eed23e0ddc5d34
Aarron Szalacinski <ASzalacinski@corporatespending.com>
2020-01-07 13:47:39 -0500

    lesson 27

--- app.ts
+++ app.ts
@@ -3,10 +3,15 @@ function add(n1: number, n2: number) {
 }
 
 function printResult(num: number): void {
-    console.log('Result: ' + num);
+  console.log("Result: " + num);
 }
 
-printResult(add(5,12));
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
+printResult(add(5, 12));
 
 let combineValues: (a: number, b: number) => number;
 
@@ -15,3 +20,7 @@ combineValues = add;
 //combineValues = 4;
 
 console.log(combineValues(8, 8));
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+});
