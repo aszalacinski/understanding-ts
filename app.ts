d7671bceb8dad00e33a46beffccfce1ed41b39f5
Aarron Szalacinski <ASzalacinski@corporatespending.com>
2020-01-07 10:33:54 -0500

    lesson 22

--- app.ts
+++ app.ts
@@ -1,18 +1,24 @@
-function combine(input1: number | string, input2: number | string) {
+function combine(input1: number | string, input2: number | string, resultConversion: 'as-number' | 'as-text') {
     let result;
-    if(typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
+    if(typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+        result = +input1 + +input2;
     }
     else {
         result = input1.toString() + input2.toString();
     }
+    // if(resultConversion === 'as-number') {
+    //     return +result;
+    // } else {
+    //     return result.toString();
+    // }
     return result;
 }
 
-const combinedAges = combine(44, 50);
-
+const combinedAges = combine(44, 50, 'as-number');
 console.log(combinedAges);
 
-const combinedNames = combine('Aarron', 'Tammy');
+const combinedStringAges = combine('44', '50', 'as-number');
+console.log(combinedAges);
 
+const combinedNames = combine('Aarron', 'Tammy', 'as-text');
 console.log(combinedNames);
