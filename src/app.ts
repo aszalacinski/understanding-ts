8b5a2f7df29d7e6c3491ed1da65aac8edd8198a8
Aarron Szalacinski <ASzalacinski@corporatespending.com>
2020-01-08 09:38:33 -0500

    lesson 60

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,17 @@ class Department {
     this.name = n;
   }
 
+  describe(this: Department) {
+    console.log('Department: ' + this.name);
+  }
+
 }
 
 const accounting = new Department('Accounting');
 
-console.log(accounting);
+accounting.describe();
+
+const accountingCopy = { name: 'Dummy', describe: accounting.describe };
+
+accountingCopy.describe();
+
