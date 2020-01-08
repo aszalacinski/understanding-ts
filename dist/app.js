387ae2a759ffa02ca5a66d8a9450fea81094d3f0
Aarron Szalacinski <ASzalacinski@corporatespending.com>
2020-01-08 16:43:44 -0500

    lesson 83

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,46 @@
 "use strict";
 const e1 = {
-    name: 'Aarron',
-    privileges: ['create-server'],
+    name: "Aarron",
+    privileges: ["create-server"],
     startDate: new Date()
 };
+function add(a, b) {
+    if (typeof a === "string" || typeof b === "string") {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+function printEmployeeInformation(emp) {
+    console.log("Name: " + emp.name);
+    if ("privileges" in emp) {
+        console.log("Privileges: " + emp.privileges);
+    }
+    if ("startDate" in emp) {
+        console.log("StartDate: " + emp.startDate);
+    }
+}
+printEmployeeInformation(e1);
+class Car {
+    drive() {
+        console.log("Driving...");
+    }
+}
+class Truck {
+    drive() {
+        console.log("Driving a truck...");
+    }
+    loadCargo(amount) {
+        console.log("Loading cargo..." + amount);
+    }
+}
+const v1 = new Car();
+const v2 = new Truck();
+function useVehicle(vehicle) {
+    vehicle.drive();
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(1000);
+    }
+}
+useVehicle(v1);
+useVehicle(v2);
 //# sourceMappingURL=app.js.map
