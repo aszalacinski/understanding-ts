4889ef0e59690e84da3b8fa2c39c35765f0cdbef
Aarron Szalacinski <ASzalacinski@corporatespending.com>
2020-01-09 15:41:06 -0500

    lesson 87

--- dist/app.js
+++ dist/app.js
@@ -10,57 +10,6 @@ function add(a, b) {
     }
     return a + b;
 }
-function printEmployeeInformation(emp) {
-    console.log("Name: " + emp.name);
-    if ("privileges" in emp) {
-        console.log("Privileges: " + emp.privileges);
-    }
-    if ("startDate" in emp) {
-        console.log("StartDate: " + emp.startDate);
-    }
-}
-printEmployeeInformation(e1);
-class Car {
-    drive() {
-        console.log("Driving...");
-    }
-}
-class Truck {
-    drive() {
-        console.log("Driving a truck...");
-    }
-    loadCargo(amount) {
-        console.log("Loading cargo..." + amount);
-    }
-}
-const v1 = new Car();
-const v2 = new Truck();
-function useVehicle(vehicle) {
-    vehicle.drive();
-    if (vehicle instanceof Truck) {
-        vehicle.loadCargo(1000);
-    }
-}
-useVehicle(v1);
-useVehicle(v2);
-function moveAnimal(animal) {
-    let speed;
-    switch (animal.type) {
-        case "bird":
-            speed = animal.flyingSpeed;
-            break;
-        case "horse":
-            speed = animal.runningSpeed;
-    }
-    console.log("Moving with speed: " + speed);
-}
-moveAnimal({ type: "bird", flyingSpeed: 10 });
-const userInputElement = document.getElementById("user-input");
-if (userInputElement) {
-    userInputElement.value = "Hi there!";
-}
-const errorBag = {
-    email: 'Not a valid email!',
-    username: 'Must start with a capital character'
-};
+const result = add('Aarron', ' Szalacinski');
+result.split(' ');
 //# sourceMappingURL=app.js.map
