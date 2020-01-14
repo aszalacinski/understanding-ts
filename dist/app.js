ee36d67cd11e965b482308b5c27709a4505f7085
Aarron Szalacinski <ASzalacinski@corporatespending.com>
2020-01-14 16:31:13 -0500

    lesson 107

--- dist/app.js
+++ dist/app.js
@@ -9,13 +9,16 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 function Logger(logString) {
+    console.log('LOGGER FACTORY');
     return function (constructor) {
         console.log(logString);
         console.log(constructor);
     };
 }
 function WithTemplate(template, hookId) {
+    console.log('TEMPLATE FACTORY');
     return function (constructor) {
+        console.log('Rendering template.');
         const hookEl = document.getElementById(hookId);
         const p = new constructor();
         if (hookEl) {
@@ -31,7 +34,7 @@ let Person = class Person {
     }
 };
 Person = __decorate([
-    Logger('LOGGING - PERSON'),
+    Logger('LOGGING'),
     WithTemplate('<h1>My Person Object</h1>', 'app'),
     __metadata("design:paramtypes", [])
 ], Person);
