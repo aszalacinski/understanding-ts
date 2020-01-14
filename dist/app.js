f7363eb30a58d5c9a80f5eee88602c9b8ffac24c
Aarron Szalacinski <ASzalacinski@corporatespending.com>
2020-01-14 15:11:26 -0500

    lesson 95

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,7 @@
 "use strict";
+function merge(objA, objB) {
+    return Object.assign(objA, objB);
+}
+const mergedObj = merge({ name: 'Aarron', hobbies: ['Work'] }, { age: 44 });
+console.log(mergedObj);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -13,11 +13,9 @@
 
 
 
-function merge<T, U>(objA: T, objB: U) {
+function merge<T extends object, U extends object>(objA: T, objB: U) {
     return Object.assign(objA, objB);
 }
 
-
-
-const mergedObj = merge<{name: string, hobbies: string[]}, {age: number}>({name: 'Aarron', hobbies: ['Work']}, {age: 44});
+const mergedObj = merge({name: 'Aarron', hobbies: ['Work']}, {age: 44});
 console.log(mergedObj);
